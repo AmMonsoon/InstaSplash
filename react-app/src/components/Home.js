e417522db83332d342bad1a1bf9ea5a8cede6710
cpowers1203 <81358233+cpowers1203@users.noreply.github.com>
2021-09-14 11:23:29 -0500

    all is rendered in the upside down

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getFollow } from '../store/image'
+import { NavLink } from 'react-router-dom'
+
 
 function Home(){
     const user = useSelector(state => state.session.user)
     const dispatch = useDispatch()
+    const followingImages = useSelector(state => Object.values(state.images.following))
 
     useEffect(() => {
         (async () => {
@@ -14,9 +17,21 @@ function Home(){
     
 
     return (
-        <h1>
-            test
-        </h1>
+        
+        <div className='following__wrapper'>
+            <div className="following__allFollowing">
+                {followingImages.map((image) => 
+                    // {console.log(image)}
+                    <div className="following__singleImage--container" key={image.id}>
+                        <NavLink to={`/images/${image.id}`}>
+                            <div className="following__singlImage--image">
+                                <img src={image.imageUrl} alt=""></img>
+                            </div>
+                        </NavLink>
+                    </div>
+                )}
+            </div>
+       </div>
         // <>
         //     {console.log('user' + user)}
         //     <h1>
