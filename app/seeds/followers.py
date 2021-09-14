ba7513e3aa0d898ccf1e86d9be4187e76ef6e0f4
aAJacob <wellerjay118@gmail.com>
2021-09-14 10:18:49 -0500

    seeding for 3 total

--- app/seeds/followers.py
+++ app/seeds/followers.py
@@ -2,8 +2,12 @@ from app.models import db, Follower
 
 def seed_followers():
     first_follow = Follower(follower=3, followed=2)
+    second = Follower(follower=2, followed=1)
+    third = Follower(follower=1, followed=3)
 
     db.session.add(first_follow)
+    db.session.add(second)
+    db.session.add(third)
     db.session.commit()
 
 def undo_followers():
--- app/seeds/images.py
+++ app/seeds/images.py
@@ -3,11 +3,21 @@ from datetime import datetime
 
 def seed_images():
     puppy_image = Image(
-        userId=2, imageUrl="https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80", 
+        userId=2, imageUrl="https://images.unsplash.com/photo-1530281700549-e82e7bf110d6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80",
         caption="What a cute puppy", profilePic=False, created_at=datetime.now()
+    )
+    apples = Image(
+        userId=1, imageUrl="https://images.unsplash.com/photo-1631347532114-bb57aef55997?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80",
+        caption="Eat healthy", profilePic=False, created_at=datetime.now()
+    )
+    bird_plant = Image(
+        userId=3, imageUrl="https://images.unsplash.com/photo-1631214953530-0ae0277ccf7f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+        caption="Is it a bird or a plant?!", profilePic=False, created_at=datetime.now()
     )
 
     db.session.add(puppy_image)
+    db.session.add(apples)
+    db.session.add(bird_plant)
     db.session.commit()
 
 def undo_images():
